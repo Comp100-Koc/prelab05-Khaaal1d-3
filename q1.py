d46126e30e8bdf8ce8cbbf6d0dc84d65a251a6fc
Khaaal1d <160760492+Khaaal1d@users.noreply.github.com>
2026-04-02 21:40:08 +0400

    Updated q1.py

--- q1.py
+++ q1.py
@@ -1,5 +1,16 @@
+def check_palindrome(text):
+    return text == text[::-1]
+
+
 def longest_palindromic_substring(s):
-    """
-    Given a string find the longest palindromic substring
-    """
-    pass
+    longest = ""
+
+    for start in range(len(s)):
+        for end in range(start + 1, len(s)):
+            current = s[start:end + 1]
+
+            if check_palindrome(current):
+                if len(current) > len(longest):
+                    longest = current
+
+    return longest
