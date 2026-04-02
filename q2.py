36776d9d317f0c9cd02ef60ea17a45ef279fdb2b
Khaaal1d <160760492+Khaaal1d@users.noreply.github.com>
2026-04-02 22:19:33 +0400

    Updated q2.py

--- q2.py
+++ q2.py
@@ -1,5 +1,16 @@
 def remove_adjacent_duplicates(s):
-    '''
-    Given a string remove all the adjacent duplicate characters and return the string
-    '''
-    pass
+    changed = True
+
+    while changed:
+        changed = False
+        i = 0
+
+        while i < len(s) - 1:
+            if s[i] == s[i + 1]:
+                s = s[:i] + s[i + 2:]
+                changed = True
+                i = 0
+            else:
+                i += 1
+
+    return s
